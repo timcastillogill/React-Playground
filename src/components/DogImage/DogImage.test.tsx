c2318ac1eed8cba5adee8e83dd88d8fa4f751322
timcastillogill <timgillviola@gmail.com>
2023-02-22 11:42:52 +0000

    Added mocks to DogImage for the hook event

--- src/components/DogImage/DogImage.test.tsx
+++ src/components/DogImage/DogImage.test.tsx
@@ -1,19 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import DogImage from "./DogImage";
+import useDogImage from "../../Hooks/useDogImage";
+
+jest.mock("../../Hooks/useDogImage");
+const mockedDogImage = jest.mocked<typeof useDogImage>(useDogImage);
 
 describe("Given a dog component is rendered", () => {
-    test("the text 'click me' is rendered on the screen", () => {
-        render(<DogImage />);
-
-        const clickMeText = screen.getByRole("heading", { level: 3 });
-        expect(clickMeText).toBeInTheDocument();
+  beforeEach(() => {
+    const DOG_IMAGE_URL = "www.whatadog.com";
+    mockedDogImage.mockReturnValue({
+      isLoading: false,
+      hasError: false,
+      dogImage: DOG_IMAGE_URL,
+      fetchDogImage: jest.fn(),
     });
+  });
 
-    test("an image is rendered on screen when component is mounted", () => {
-        render(<DogImage />);
+  test("the text 'click me' is rendered on the screen", () => {
+    render(<DogImage />);
 
-        const dogImage = screen.getByRole("button");
-        expect(dogImage).toBeInTheDocument();
-    });
+    const clickMeText = screen.getByRole("heading", { level: 3 });
+    expect(clickMeText).toBeInTheDocument();
+  });
+
+  test("an image is rendered on screen when component is mounted", () => {
+    render(<DogImage />);
+
+    const dogImage = screen.getByRole("button");
+    expect(dogImage).toBeInTheDocument();
+  });
 });
