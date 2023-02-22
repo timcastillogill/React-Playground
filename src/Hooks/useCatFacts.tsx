ea4435e942cea6db96baa1deffe06e54c7c2f4e4
timcastillogill <timgillviola@gmail.com>
2023-02-22 11:24:15 +0000

    Extracted the fetch method out of the useEffect

--- src/Hooks/useCatFacts.tsx
+++ src/Hooks/useCatFacts.tsx
@@ -5,19 +5,20 @@ const useCatFacts = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    setHasError(false);
+    try {
+      const res = await fetch("https://catfact.ninja/facts");
+      const json = await res.json();
+      setData(json.data);
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      setHasError(false);
-      try {
-        const res = await fetch("https://catfact.ninja/facts");
-        const json = await res.json();
-        setData(json.data);
-      } catch (error) {
-        setHasError(true);
-      }
-      setIsLoading(false);
-    };
     fetchData();
   }, [setData]);
 
