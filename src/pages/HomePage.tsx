1950b19588ed4161f5a6d3e49c763701f0d812a5
timcastillogill <timgillviola@gmail.com>
2023-02-23 15:02:32 +0000

    Added buttons and dom links to components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { routerPaths } from "../routes/paths";
 
 const HomePage = () => {
-  return <h1>This is the home page</h1>;
+  return (
+    <section>
+      <h2>This is the home page</h2>
+      <button>
+        <Link to={routerPaths.catFact}>This way to Cat Facts 👉</Link>
+      </button>
+      <button>
+        <Link to={routerPaths.dogImage}>This way to Dog Images 👉</Link>
+      </button>
+    </section>
+  );
 };
 
 export default HomePage;
