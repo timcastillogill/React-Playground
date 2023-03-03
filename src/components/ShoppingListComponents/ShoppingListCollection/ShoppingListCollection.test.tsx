88581c107ff1d873e5450efb73ab4953e77d78bf
timcastillogill <timgillviola@gmail.com>
2023-03-03 16:07:07 +0000

    Cleaned up this test file

--- src/components/ShoppingListComponents/ShoppingListCollection/ShoppingListCollection.test.tsx
+++ src/components/ShoppingListComponents/ShoppingListCollection/ShoppingListCollection.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("Given the user has added text to the input", () => {
-  test("when there are no items in the shopping list an message will show", () => {
+  test("then the user can clear the list with an onscreen button", () => {
     render(<ShoppingListCollection />);
     userEvent.click(
       screen.getByRole("button", { name: /clear shopping list/i })
@@ -12,16 +12,15 @@ describe("Given the user has added text to the input", () => {
     const emptyShoppingListMessage = screen.getByText(/no items in the list/i);
     expect(emptyShoppingListMessage).toBeInTheDocument();
   });
+});
 
-  test("then the user can clear the list with an onscreen button", () => {
+describe("Given the user has not added anything to their list", () => {
+  test("when there are no items in the shopping list a message will show", () => {
     render(<ShoppingListCollection />);
-
-    const clearListButton = screen.getByRole("button", {
-      name: /clear shopping list/i,
-    });
-    userEvent.click(clearListButton);
-    const nothingInListMessage = screen.getByText(/no items in the list/i);
-
-    expect(nothingInListMessage).toBeInTheDocument();
+    userEvent.click(
+      screen.getByRole("button", { name: /clear shopping list/i })
+    );
+    const emptyShoppingListMessage = screen.getByText(/no items in the list/i);
+    expect(emptyShoppingListMessage).toBeInTheDocument();
   });
 });
