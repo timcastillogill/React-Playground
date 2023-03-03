15f1d157974fd48145147dc574db8ada8fd916df
timcastillogill <timgillviola@gmail.com>
2023-03-03 15:55:39 +0000

    Added test for no items in list

--- src/components/ShoppingListComponents/ShoppingListCollection/ShoppingListCollection.test.tsx
+++ src/components/ShoppingListComponents/ShoppingListCollection/ShoppingListCollection.test.tsx
@@ -12,4 +12,16 @@ describe("Given the user has added text to the input", () => {
     const emptyShoppingListMessage = screen.getByText(/no items in the list/i);
     expect(emptyShoppingListMessage).toBeInTheDocument();
   });
+
+  test("then the user can clear the list with an onscreen button", () => {
+    render(<ShoppingListCollection />);
+
+    const clearListButton = screen.getByRole("button", {
+      name: /clear shopping list/i,
+    });
+    userEvent.click(clearListButton);
+    const nothingInListMessage = screen.getByText(/no items in the list/i);
+
+    expect(nothingInListMessage).toBeInTheDocument();
+  });
 });
