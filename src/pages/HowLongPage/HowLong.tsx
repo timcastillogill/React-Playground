5169243a1dbed8640c251f5d66c658647f1d81b4
timcastillogill <timgillviola@gmail.com>
2023-03-21 06:41:15 -0500

    Moved the input component to the collection

--- src/pages/HowLongPage/HowLong.tsx
+++ src/pages/HowLongPage/HowLong.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import HowLongInput from "../../components/HowLongComponents/HowLongInput/HowLongInput";
 import HowLongCollection from "../../components/HowLongComponents/HowLongCollection/HowLongCollection";
 
 const HowLongPage = () => {
   return (
     <section>
-      <HowLongInput />
       <HowLongCollection />
     </section>
   );
