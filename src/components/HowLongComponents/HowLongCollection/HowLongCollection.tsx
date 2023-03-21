d2708bb81c614132cb49acfda1378d8df1cf1606
timcastillogill <timgillviola@gmail.com>
2023-03-21 06:41:30 -0500

    Added state and mapped over the default values

--- src/components/HowLongComponents/HowLongCollection/HowLongCollection.tsx
+++ src/components/HowLongComponents/HowLongCollection/HowLongCollection.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import HowLongInput from "../HowLongInput/HowLongInput";
 
 const HowLongCollection = () => {
+  const [events, setEvents] = useState([
+    { id: 1, eventName: "My Birthday", date: "2023-02-23" },
+  ]);
+
   return (
-    <ul>
-      <li>My Birthday</li>
-    </ul>
+    <section>
+      <HowLongInput />
+      <ul>
+        {events.map((event) => (
+          <li>
+            {event.eventName} | {event.date}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
