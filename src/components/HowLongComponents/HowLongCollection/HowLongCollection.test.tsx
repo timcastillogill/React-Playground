2212dcbfbcf336f1d7a4a1f1aa37f3231e206e61
timcastillogill <timgillviola@gmail.com>
2023-03-22 12:19:18 -0500

    Updates to handle event test

--- src/components/HowLongComponents/HowLongCollection/HowLongCollection.test.tsx
+++ src/components/HowLongComponents/HowLongCollection/HowLongCollection.test.tsx
@@ -8,16 +8,29 @@ const mockEventInput = jest.mocked<typeof HowLongInput>(HowLongInput);
 
 describe("Given the user has inputted an event into the input", () => {
   beforeEach(() => {
-    const inputtedEventName = "My Birthday";
-    const inputtedEventDate = "2024-02-23";
-    mockEventInput.mockReturnValue(inputtedEventName);
-    jest.clearAllMocks();
+    // const inputtedEventName = "My Birthday";
+    // const inputtedEventDate = "2024-02-23";
+    // mockEventInput.mockReturnValue(inputtedEventName);
+    // jest.clearAllMocks();
   });
 
   test("Then the user will see a list of event NAMES", () => {
     render(<HowLongCollection />);
 
+    const inputtedEventName = "My Birthday";
+    const inputtedEventDate = "2024-02-23";
+    const handleNewEvent = jest.fn();
+    // expect(handleNewEvent).toHaveBeenCalledWith(
+    //   inputtedEventName,
+    //   inputtedEventDate
+    // );
+    handleNewEvent.apply(inputtedEventName, inputtedEventDate);
+
     const eventList = screen.getByRole("list");
-    expect(eventList.firstChild.innerHTML).toBe("My Birthday | 2024-02-23");
+    const eventItem = screen.getByText("New Event | 2022-04-01");
+    expect(eventList).toContainElement(eventItem);
+
+    // const eventList = screen.getByRole("list");
+    // expect(eventList.firstChild!.innerHTML).toBe("My Birthday | 2024-02-23");
   });
 });
