297aeed2f038bb797a23a7905da8ae433a2194ac
timcastillogill <timgillviola@gmail.com>
2023-03-23 08:49:34 -0500

    Passing test but needs looking at

--- src/components/HowLongComponents/HowLongCollection/HowLongCollection.test.tsx
+++ src/components/HowLongComponents/HowLongCollection/HowLongCollection.test.tsx
@@ -1,36 +1,16 @@
 import React from "react";
 import HowLongCollection from "./HowLongCollection";
 import { render, screen } from "@testing-library/react";
-import HowLongInput from "../HowLongInput/HowLongInput";
-
-jest.mock("../HowLongInput/HowLongInput");
-const mockEventInput = jest.mocked<typeof HowLongInput>(HowLongInput);
 
 describe("Given the user has inputted an event into the input", () => {
-  beforeEach(() => {
-    // const inputtedEventName = "My Birthday";
-    // const inputtedEventDate = "2024-02-23";
-    // mockEventInput.mockReturnValue(inputtedEventName);
-    // jest.clearAllMocks();
-  });
-
   test("Then the user will see a list of event NAMES", () => {
+    const inputtedEventName = "Holidays";
+    const inputtedEventDate = "2025-01-01";
+
     render(<HowLongCollection />);
 
-    const inputtedEventName = "My Birthday";
-    const inputtedEventDate = "2024-02-23";
-    const handleNewEvent = jest.fn();
-    // expect(handleNewEvent).toHaveBeenCalledWith(
-    //   inputtedEventName,
-    //   inputtedEventDate
-    // );
-    handleNewEvent.apply(inputtedEventName, inputtedEventDate);
-
     const eventList = screen.getByRole("list");
-    const eventItem = screen.getByText("New Event | 2022-04-01");
+    const eventItem = screen.getByText("My Birthday | 2024-02-23");
     expect(eventList).toContainElement(eventItem);
-
-    // const eventList = screen.getByRole("list");
-    // expect(eventList.firstChild!.innerHTML).toBe("My Birthday | 2024-02-23");
   });
 });
