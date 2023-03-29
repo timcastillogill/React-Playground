6cd75f44859979033f48cd21b3303acf3d6ea421
timcastillogill <timgillviola@gmail.com>
2023-03-29 07:26:54 -0500

    Convert test to paramaterized test

--- src/components/HowLongUntil/HowLongUntil.test.tsx
+++ src/components/HowLongUntil/HowLongUntil.test.tsx
@@ -4,32 +4,16 @@ import userEvent from "@testing-library/user-event";
 import HowLongUntil from "./HowLongUntil";
 
 describe("HowLongUntil", () => {
-  describe("Given the user has typed 'Hello World' and hit a button", () => {
-    test("Then it will display 'Hello World!'", () => {
-      render(<HowLongUntil />);
+  describe.each([["Hello World"], ["Laurence's Birthday"], ["Tim's Birthday"]])(
+    "Given the user has typed '%s' and hit a button",
+    (text) => {
+      test(`Then it will display '${text}'`, () => {
+        render(<HowLongUntil />);
 
-      userEvent.type(screen.getByRole("textbox"), "Hello World!");
-      userEvent.click(screen.getByRole("button"));
-      expect(screen.getByText("Hello World!")).toBeInTheDocument();
-    });
-  });
-  describe("Given the user has typed 'Laurence's Birthday' and hit a button", () => {
-    test("Then it will display 'Laurence's Birthday'", () => {
-      render(<HowLongUntil />);
-
-      userEvent.type(screen.getByRole("textbox"), "Laurence's Birthday");
-      userEvent.click(screen.getByRole("button"));
-      expect(screen.getByText("Laurence's Birthday")).toBeInTheDocument();
-    });
-  });
-
-  describe("Given the user had typed 'Tim's Birthday' and hit a button", () => {
-    test("Then it will display 'Tim's Birthday'", () => {
-      render(<HowLongUntil />);
-
-      userEvent.type(screen.getByRole("textbox"), "Tim's Birthday");
-      userEvent.click(screen.getByRole("button"));
-      expect(screen.getByText("Tim's Birthday")).toBeInTheDocument();
-    });
-  });
+        userEvent.type(screen.getByRole("textbox"), text);
+        userEvent.click(screen.getByRole("button"));
+        expect(screen.getByText(text)).toBeInTheDocument();
+      });
+    }
+  );
 });
