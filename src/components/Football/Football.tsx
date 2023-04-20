37365a9acc932359e1cbf13d70b663598cf63b08
timcastillogill <timgillviola@gmail.com>
2023-04-20 15:45:10 +0100

    Added conditionals for error and loading renderings

--- src/components/Football/Football.tsx
+++ src/components/Football/Football.tsx
@@ -13,18 +13,24 @@ const Football = () => {
       <button className="ui-button" onClick={getFootballCompInfo}>
         Get Info
       </button>
-      <table>
-        <tbody>
-          <tr>
-            <th>Debut</th>
-            <th>Team Name</th>
-          </tr>
-          <tr>
-            <td>{data.debut}</td>
-            <td>{data.name}</td>
-          </tr>
-        </tbody>
-      </table>
+      {hasError && (
+        <h3>Something hasn't gone to plan. Please click the button again</h3>
+      )}
+      {isLoading && !hasError && <h2>Loading...</h2>}
+      {!hasError && !isLoading && (
+        <table>
+          <tbody>
+            <tr>
+              <th>Debut</th>
+              <th>Team Name</th>
+            </tr>
+            <tr>
+              <td>{data.debut}</td>
+              <td>{data.name}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </section>
   );
 };
