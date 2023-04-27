ce81f2851af6f59e048f2def5b898b167bb0d080
timcastillogill <timgillviola@gmail.com>
2023-04-27 13:30:39 +0100

    Updated imports

--- src/components/Football/FootballStandingsInformation/FootballStandingsInformation.tsx
+++ src/components/Football/FootballStandingsInformation/FootballStandingsInformation.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import useFootballStandings from "../../../Hooks/useFootballStandings";
 import "./FootballStandingsInformation.css";
 
