1730ec5e3ea02e9868a35b894ef7eac3dd012d1b
timcastillogill <timgillviola@gmail.com>
2023-07-21 16:10:13 +0100

    Add asterix to redirect to main when no path

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,7 +1,7 @@
 export const baseURL = process.env.REACT_APP_BASE_URL;
 
 export const routerPaths = {
-  noPath: "/",
+  noPath: "*",
   home: "/home",
   catFact: "/catFact",
   dogImage: "/dogImage",
