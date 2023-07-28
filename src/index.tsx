ff11a61908432e4e78253e6636a90cdb9dc21394
timcastillogill <timgillviola@gmail.com>
2023-07-28 15:48:58 +0100

    Debug

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,13 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 root.render(
   <React.StrictMode>
     <App />
