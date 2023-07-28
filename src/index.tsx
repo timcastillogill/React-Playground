17a66496214b4792e84778f8ec080cce4ffb95e0
timcastillogill <timgillviola@gmail.com>
2023-07-28 15:26:18 +0100

    create hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, RouterProvider, createHashRouter } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,6 +18,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <RouterProvider router={router} />
       <App />
     </HashRouter>
   </React.StrictMode>
